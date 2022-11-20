99c1c363a3ae620d6321924607b87bb0d36940eb
jakeduffy <duffyjemmett@gmail.com>
2022-11-20 13:49:26 -0500

    updated java main

--- java_chess/src/Main.java
+++ java_chess/src/Main.java
@@ -1,16 +1,24 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
     Color agent = Color.WHITE;
     ChessState c = new ChessState();
-    SearchAgent s = new SearchAgent(5);
-    System.out.println(c);
-    for (int i = 0; i < 50; i++) {
-      List<Action> m = c.get_legal_moves(agent);
-      Action a = s.get_action(c, agent);
-      c = c.get_successor_state(a, agent);
-      System.out.println(c);
+//    SearchAgent s = new SearchAgent(5);
+//    System.out.println(c);
+    List<ChessState> states = new ArrayList<>();
+    states.add(c);
+    for (int i = 0; i < 5; i++) {
+      List<ChessState> newStates = new ArrayList<>();
+      for (ChessState state : states) {
+        List<Action> moves = state.get_legal_moves(agent);
+        for (Action m : moves) {
+          newStates.add(state.get_successor_state(m, agent));
+        }
+      }
+      System.out.println(newStates.size());
+      states = newStates;
       agent = agent.get_opposite();
     }
   }
