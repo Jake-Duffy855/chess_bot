accb1c0ec59da124a47ed75ee8902f1134bbc0c7
jakeduffy <duffyjemmett@gmail.com>
2022-12-01 20:47:31 -0500

    uhhh

--- View/render_board.py
+++ View/render_board.py
@@ -36,7 +36,7 @@ tiles = [((x * ts, y * ts, ts, ts), c1 if (x + y) % 2 == 0 else c2) for x in ran
 [pygame.draw.rect(background, color, rect) for rect, color in tiles]
 screen.blit(background, (0, 0))
 
-chess_state = ChessState(DEFAULT_BOARD)
+chess_state = JavaChessState(DEFAULT_BOARD)
 search_agent = JavaSearchAgent(depth=4)
 auto_move = False
 player = Color.WHITE
