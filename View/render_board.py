db6ca0fd7fbd2b9f470cc6dc153eb6dbb16bc927
jakeduffy <duffyjemmett@gmail.com>
2022-12-04 02:28:31 -0500

    tidied render

--- View/render_board.py
+++ View/render_board.py
@@ -206,7 +206,7 @@ while is_running:
     else:
         clock.tick(60)
     # sleep(0.5)
-    if auto_move: # and player == Color.BLACK and not chess_state.is_end_state(player):
+    if not chess_state.is_end_state(player) and auto_move: # and player == Color.BLACK:
         best_move = search_agent.get_action(chess_state, player)
         chess_state = chess_state.get_successor_state(best_move, player)
         last_move = best_move
